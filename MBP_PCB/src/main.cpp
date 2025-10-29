88abf92b524d8ffeeb30e7464b36fba4216e817a
Deelaka Dias <deelakadias2002@gmail.com>
2025-10-29 08:50:34 +0530

    Update main.cpp

--- MBP_PCB/src/main.cpp
+++ MBP_PCB/src/main.cpp
@@ -77,7 +77,10 @@ void loop()
 {
   int16_t x, y, z;
 
-  digitalWrite(7, HIGH); // Set pin 8 high to indicate measurement start
+  // digitalWrite(38, HIGH);  // Turn ON pin 38 Buzzer pin
+  // delay(3000);             // Wait for 3 seconds (3000 ms)
+  // digitalWrite(38, LOW); // Turn OFF pin 38 Buzzer pin
+  // delay(3000);  // Wait for 3 seconds (3000 ms)
 
   Wire.beginTransmission(LIS3DHTR_ADDR);
   Wire.write(LIS3DHTR_OUT_X_L | 0x80); // auto-increment
@@ -102,15 +105,15 @@ void loop()
   // Print
   char buffer[256];
   sprintf(buffer, "X: %+07.3f g  Y: %+07.3f g  Z: %+07.3f g | Vibration: %07.3f g", x_g, y_g, z_g, vibration);
-  // if (vibration > 0.7f)
-  // {
-  //   Serial.println("High vibration detected!");
-  //   digitalWrite(38, HIGH); // Set pin 37 high if vibration > 1g
-  // }
-  // else
-  // {
-  //   digitalWrite(38, LOW); // Set pin 8 low if vibration <= 1g
-  // }
+  if (vibration > 0.7f)
+  {
+    Serial.println("High vibration detected!");
+    digitalWrite(38, HIGH); // Set pin 37 high if vibration > 1g
+  }
+  else
+  {
+    digitalWrite(38, LOW); // Set pin 8 low if vibration <= 1g
+  }
 
   Serial.println(buffer);
 
